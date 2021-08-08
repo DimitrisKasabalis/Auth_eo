f1a5ab4558f0c98b73b29d830ee379e970c5fe80
vesnikos <vesnikos@gmail.com>
2021-08-08 14:46:56 +0100

    added nc utilities to eo_engine image

--- build.ps1
+++ build.ps1
@@ -1,10 +1,7 @@
 param ([Switch]$push)
-$VERSION = '0.2.0'
 
 Invoke-Expression -Command "docker compose build worker"
-Invoke-Expression -Command "docker tag vesnikos/eo_engine:${VERSION} vesnikos/eo_engine:latest"
 
 if ($push) {
-    Invoke-Expression -Command "docker push vesnikos/eo_engine:latest"
-    Invoke-Expression -Command "docker push vesnikos/eo_engine:${VERSION}"
+    Invoke-Expression -Command "docker push vesnikos/eo_engine"
 }
